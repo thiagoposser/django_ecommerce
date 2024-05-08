06fa121a48d359e211f0b5ad3dff02abc95642e9
Thiago Posser da Silva <thiagoposser@yahoo.com.br>
2024-05-08 11:37:02 -0300

    creat context

--- src/e_commerce/urls.py
+++ src/e_commerce/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from .views import home, about, contact
 urlpatterns = [
         path('', home),
         path('admin/', admin.site.urls),
+        path('about/', about),
+	    path('contact/', contact),
 ]
--- src/e_commerce/views.py
+++ src/e_commerce/views.py
@@ -1,4 +1,27 @@
+# from django.http import HttpResponse
+# from django.shortcuts import render
+# def home(request):
+#     return render(request, "home.html", {})
+
 from django.http import HttpResponse
 from django.shortcuts import render
 def home(request):
-    return render(request, "home.html", {})
+    context = {
+        "title": "Página principal",
+        "content": "Bem-vindo a página principal"
+    }
+    return render(request, "home.html", context)
+
+def about(request):
+    context = {
+        "title": "Página sobre",
+        "content": "Bem-vindo a página sobre"
+    }
+    return render(request, "about/view.html", context)
+
+def contact(request):
+    context = {
+        "title": "Página de contato",
+        "content": "Bem-vindo a página de contato"
+    }
+    return render(request, "contact/view.html", context)

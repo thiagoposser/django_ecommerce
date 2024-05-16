741b39076a353e55777a43168a5e523f57a65d04
Thiago Posser da Silva <thiagoposser@yahoo.com.br>
2024-05-16 15:39:29 -0300

    login user

--- src/e_commerce/urls.py
+++ src/e_commerce/urls.py
@@ -1,9 +1,32 @@
+"""e_commerce URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path
-from .views import home, about, contact
+
+from .views import home_page, about_page, contact_page, login_page, register_page
+
 urlpatterns = [
-        path('', home),
-        path('admin/', admin.site.urls),
-        path('about/', about),
-	    path('contact/', contact),
+	path('', home_page),
+	path('about/', about_page),
+	path('contact/', contact_page),
+        path('login/', login_page),
+        path('register/', register_page),
+	path('admin/', admin.site.urls),
 ]
